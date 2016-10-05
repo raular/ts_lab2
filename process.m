67dd0ab6b05b11102fa958fc3b8f285ccaeafeba
raular <raulalonsorodriguez96@gmail.com>
2016-10-06 00:43:08 +0200

    c

--- process.m
+++ process.m
@@ -5,17 +5,13 @@ H = fft(h,N);
 M = N - L + 1;
 P = floor(Lx/M);
 y=zeros(1, P*M+N);
-if P > 0
-    for i = 0 : P - 1
-       xb = x(i*M+1:(i+1)*M);
-       yb = cc(xb,H);
-       y(i*M+1:i*M+N) = y(i*M+1:i*M+N) + yb;
-    end
-    xb = x(P*M+1:Lx);
+for i = 0 : P - 1
+    xb = x(i*M+1:(i+1)*M);
     yb = cc(xb,H);
-    y(P*M+1:P*M+N) = y(P*M+1:P*M+N) + yb;
-else
-    y = cc(x,H);
+    y(i*M+1:i*M+N) = y(i*M+1:i*M+N) + yb;
 end
+xb = x(P*M+1:Lx);
+yb = cc(xb,H);
+y(P*M+1:P*M+N) = y(P*M+1:P*M+N) + yb;
 
 end
